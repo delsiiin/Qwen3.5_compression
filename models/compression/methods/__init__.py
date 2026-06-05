6da2115b09a148f8e973e12a9a572aa438fb154f
Minwei Zhang <1749970370@qq.com>
2026-06-05 13:28:16 +0000

    update our method (snapkv_hidden_mix -- adaptive layer budget mode)

--- models/compression/methods/__init__.py
+++ models/compression/methods/__init__.py
@@ -3,10 +3,21 @@ from ..utils import cal_similarity, compute_attention_scores
 from .snapkv import SnapKV
 from .snapkv_neighbor_shared import SnapKVNeighborShared
 from .snapkv_hidden_mix import SnapKVHiddenMix
+from .snapkv_hidden_mix_layer import SnapKVHiddenMix as SnapKVHiddenMixLayer
 from .streamingllm import StreamingLLM
 from .h2o import H2O
 from .criticalkv import CriticalKV
 from .defensivekv import DefensiveKV
 from .laprox import LaProx
 
-__all__ = ["SnapKV", "SnapKVNeighborShared", "SnapKVHiddenMix", "StreamingLLM", "H2O", "CriticalKV", "DefensiveKV", "LaProx"]
+__all__ = [
+    "SnapKV",
+    "SnapKVNeighborShared",
+    "SnapKVHiddenMix",
+    "SnapKVHiddenMixLayer",
+    "StreamingLLM",
+    "H2O",
+    "CriticalKV",
+    "DefensiveKV",
+    "LaProx",
+]
