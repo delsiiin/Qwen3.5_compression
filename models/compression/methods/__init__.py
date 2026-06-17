193b32a35e8b1618f5f9556eaaaa938bd8887570
delsin <1749970370@qq.com>
2026-06-17 05:58:49 +0000

    update our method (delete cos importance)

--- models/compression/methods/__init__.py
+++ models/compression/methods/__init__.py
@@ -7,6 +7,7 @@ from .snapkv_hidden_mix import SnapKVHiddenMix
 from .snapkv_hidden_mix_ada import SnapKVHiddenMix as SnapKVAdaHiddenMixAda
 from .snapkv_hidden_mix_layer import SnapKVHiddenMix as SnapKVHiddenMixLayer
 from .snapkv_hidden_mix_neighbor import SnapKVHiddenMix as SnapKVHiddenMixNeighbor
+from .snapkv_hidden_mix_no_cos import SnapKVHiddenMix as SnapKVHiddenMixNoCos
 from .snapkv_hidden_mix_random import SnapKVHiddenMix as SnapKVHiddenMixRandom
 from .snapkv_hidden_wo_mix import SnapKVHiddenMix as SnapKVHiddenWoMix
 from .streamingllm import StreamingLLM
@@ -23,6 +24,7 @@ __all__ = [
     "SnapKVAdaHiddenMixAda",
     "SnapKVHiddenMixLayer",
     "SnapKVHiddenMixNeighbor",
+    "SnapKVHiddenMixNoCos",
     "SnapKVHiddenMixRandom",
     "SnapKVHiddenWoMix",
     "StreamingLLM",
